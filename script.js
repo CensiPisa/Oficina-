44e77e1d05217c2ff540249dae2e45a5dbad3820
Felipe Censi Pisa <146037928+CensiPisa@users.noreply.github.com>
2024-05-01 16:34:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('meuBotao').onclick = function() {
+	document.getElementById('texto').innerHTML = 'Você clicou no botão!';
+	document.getElementById('texto').style.color = 'green';
+};
